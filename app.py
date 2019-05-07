89bd8e90d951f2ccc32fe7b5221ce287a5ced95d
Mahashraya Sundararaman <mahashraya@Mahashrayas-MacBook-Pro.local>
2019-05-06 20:56:24 -0400

    render json

--- app.py
+++ app.py
@@ -8,3 +8,10 @@ import numpy as np
 import pandas as pd
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html', data = 'movies.json')
+
+if __name__ == '__main__':
+    app.run(debug = True, use_reloader = True)
